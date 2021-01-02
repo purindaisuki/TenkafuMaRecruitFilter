a17a9f5189847a7a0e9a77fb543d51d6aa0de9d1
purindaisuki <good12dave@gmail.com>
2021-01-02 23:32:07 +0800

    change design typesetting etc.

--- filter.js
+++ filter.js
@@ -19,51 +19,62 @@ document.addEventListener("DOMContentLoaded", function(){
 
 window.onload = () => {
 
-    //populate tags into drop lists
-    let tagList = document.querySelector("#tag-list")
-    fetch('./tags.json')
-    .then(response => response.json())
-    .then(tags => {
-        for (const attr of tags) {
-            attr.tags.forEach(tag => {
-                let option = document.createElement("option")
-                option.setAttribute("value", tag)
-                option.textContent = tag
-                tagList.appendChild(option)
-            });
-        }
-    });
-
     // help info button
     let helpModal = document.querySelector("#help-modal")
     document.querySelector("#help-icon").addEventListener("click", () => {
-        helpModal.style.display = "block";
+        helpModal.style.display = "block"
     })
 
-    // close modal button
-    document.querySelector("#close-modal").addEventListener("click", () => {
-        helpModal.style.display = "none";
+    // close help modal button
+    document.querySelector("#close-help-modal").addEventListener("click", () => {
+        helpModal.style.display = "none"
     })
-    
-    // close modal when user clicks outside of the modal
-    window.onclick = (event) => {
-        if (event.target == helpModal)
-            helpModal.style.display = "none";
-    } 
 
     // back to top button
     document.querySelector("#to-top-icon").addEventListener("click", () => {
-        document.body.scrollTop = 0; // For Safari
-        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+        document.body.scrollTop = 0 // For Safari
+        document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
+    })
+
+    // add select tag buttons
+    Array.from(document.querySelectorAll(".tag-container"))
+    .forEach(slot => clearTag(slot))
+
+    // select tag
+    let tagModal = document.querySelector("#tag-select-modal")
+    document.querySelectorAll(".tag-container").forEach(tagBtn => {
+        tagBtn.addEventListener("click", (event) => {
+            tagModal.style.display = "block"
+            prepareModal(event.target.closest("li").children[0])
+        })
+    })
+
+    let tagSearchElement = document.querySelector("#tag-search-input")
+    // close modal when user clicks outside of the modal
+    window.onclick = (event) => {
+        if (event.target == helpModal)
+            helpModal.style.display = "none"
+        else if (event.target == tagModal) {
+            tagModal.style.display = "none"
+            // clear user's seach
+            tagSearchElement.value = ""
+        }
+    }
+
+    // close tag select modal button
+    document.querySelector("#close-tag-modal").addEventListener("click", () => {
+        tagModal.style.display = "none"
+        tagSearchElement.value = ""
     })
 
     // filter button
     document.querySelector("#filterBtn").addEventListener("click", () => filter())
 
-    // delete tag buttons
-    document.querySelectorAll(".delete-icon").forEach(icon => icon.addEventListener("click", event => {
-        event.target.closest("div").children[0].value = ""
-    }))
+    // delete all tags
+    document.querySelector("#delete-icon").addEventListener("click", event => {
+        Array.from(document.querySelectorAll(".tag-container"))
+        .forEach(slot => clearTag(slot))
+    })
 
     const getCellValue = (tr, idx) => tr.children[idx].innerHTML
 
@@ -72,28 +83,139 @@ window.onload = () => {
     document.querySelectorAll("th").forEach((th, _, ths) => th.addEventListener("click", () => {
         // sort table
         const table = document.querySelector("#result")
-        let flag = true;
+        let isDesc = true;
         ths.forEach(thead => {
             if (thead.classList.contains("asc")) {
                 thead.classList.remove("asc")
                 if (thead == th)
-                    flag = false
+                    isDesc = false
             }
             if (thead.classList.contains("desc"))
                 thead.classList.remove("desc")
         })
-        th.classList.add(flag ? "asc" : "desc")
+        th.classList.add(isDesc ? "asc" : "desc")
         Array.from(table.querySelectorAll("tr"))
-        .sort(comparator(Array.from(ths).indexOf(th), flag))
+        .sort(comparator(Array.from(ths).indexOf(th), isDesc))
         .forEach(tr => table.appendChild(tr))
     }));
+}
+
+function prepareModal(tagSlot) {
+    //populate tags into tag select modal
+    let tagList = document.querySelector("#tag-list")
+    tagList.innerHTML = ""
+    fetch('./tags.json')
+    .then(response => response.json())
+    .then(tags => {
+        for (const attr of tags)
+            attr.tags.forEach(tag => addTag(tagSlot, tagList, attr.attribute, tag))
+
+        // search tags
+        document.querySelector("#tag-search-input")
+        .addEventListener("input", (event) => {
+            let query = event.target.value
+            tagList.innerHTML = ""
+            for (const attr of tags) {
+                attr.tags.forEach(tag => {
+                    if (tag.includes(query))
+                        addTag(tagSlot, tagList, attr.attribute, tag)
+                })
+            }
+        })
+
+        // select tab
+        Array.from(document.querySelectorAll(".tag-tab")).forEach(tab => {
+            tab.addEventListener("click", (event) => {
+                Array.from(document.querySelectorAll(".tag-tab"))
+                .forEach(t => t.classList.remove("tab-active"))
+                let tabElement = event.target.closest("li")
+                tabElement.classList.add("tab-active")
+                let tabStr = tabElement.children[0].getAttribute("id")
+                Array.from(tagList.children).forEach(tag => {
+                    if (tag.classList.contains(tabStr)) {
+                        if (tag.classList.contains("tag-hidden"))
+                            tag.classList.remove("tag-hidden")
+                    } else {
+                        if (!tag.classList.contains("tag-hidden"))
+                            tag.classList.add("tag-hidden")
+                    }
+                })
+            })
+        })
+    })
+}
+
+function addTag(slot, target, attribute, tagStr) {
+    let tag = createTagElemet(attribute, tagStr)
+    // add tag event
+    tag.addEventListener("click", () => {
+        tag.classList.add("tag")
+        slot.innerHTML = ""
+        slot.classList.remove("tag-type-container")
+        slot.classList.remove("tag-category-container")
+        slot.classList.remove("tag-race-container")
+        slot.classList.remove("tag-body-container")
+        slot.classList.remove("tag-oppai-container")
+        slot.classList.remove("tag-rank-container")
+        slot.classList.remove("tag-else-container")
+        slot.classList.add("tag-" + attribute + "-container")
+        slot.appendChild(tag)
+        let removeIcon = document.createElement("span")
+        removeIcon.innerHTML = "&times;"
+        removeIcon.setAttribute("style", "margin-right: 12px")
+        removeIcon.addEventListener("click", (event) => {
+            event.stopImmediatePropagation();
+            clearTag(slot)
+        })
+        slot.appendChild(removeIcon)
+        // close modal
+        document.querySelector("#tag-select-modal").style.display = "none"
+        document.querySelector("#tag-search-input").value = ""
+    })
+    tag.classList.add("tag-all")
+    tag.classList.add("tag-" + attribute)
+    target.appendChild(tag)
+}
+
+function createTagElemet(attribute, tagStr) {
+    // create tag icon
+    let useIcon = document.createElementNS("http://www.w3.org/2000/svg", "use")
+    useIcon.setAttributeNS(
+        "http://www.w3.org/1999/xlink",
+        "href",
+        "#" + attribute + "-icon"
+    )
+    let icon = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    icon.classList.add("filter-icon")
+    icon.classList.add("tag-icon")
+    icon.appendChild(useIcon)
+    // add tag text
+    let tag = document.createElement("div")
+    let tagText =document.createElement("span")
+    tagText.innerHTML = tagStr
+    tag.appendChild(icon)
+    tag.appendChild(tagText)
+    return tag
+}
+
+function clearTag(slot) {
+    slot.innerHTML = ""
+    slot.classList.remove("tag-type-container")
+    slot.classList.remove("tag-category-container")
+    slot.classList.remove("tag-race-container")
+    slot.classList.remove("tag-body-container")
+    slot.classList.remove("tag-oppai-container")
+    slot.classList.remove("tag-rank-container")
+    slot.classList.remove("tag-else-container")
+    let plainTag = createTagElemet("tag", "選擇標籤")
+    plainTag.classList.add("tag")
+    slot.appendChild(plainTag)
 }
 
 function filter() {
     // read tags
     const tagNum = document.querySelector("#applied-tag-num").value
-    let validTags = []
-    Array.from(document.querySelectorAll(".tag")).forEach(element => addTag(validTags, element.children[0].value))
+    let validTags = readTag()
     if (validTags.length < tagNum) {
         alert("有效標籤數量過少")
         return;
@@ -198,10 +320,15 @@ function filter() {
     });
 }
 
-function addTag(arr, input) {
-    let option = document.querySelector("#tag-list option[value='" + input + "']")
-    if (option != null && option.value.length > 0 && !arr.includes(input))
-        arr.push(input)
+function readTag() {
+    let tags = []
+    Array.from(document.querySelectorAll(".tag")).forEach(tag => {
+        tagStr = tag.children[1].innerHTML
+        if (tagStr != null && tagStr.length > 0
+            && tagStr != "選擇標籤" && !tags.includes(tagStr))
+            tags.push(tagStr)
+    })
+    return tags
 }
 
 function* combinations(elements, num) {
